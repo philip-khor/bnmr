fe7db44f51603a29121fca2a943637a2d2a356c4
Philip Khor <philip10khor@gmail.com>
2019-03-30 12:42:55 +0800

    add year option to opr()

--- R/opr.R
+++ R/opr.R
@@ -1,10 +1,20 @@
 #' Overnight Policy Rate (OPR)
 #'
 #' This function allows you to ... from the BNM API.
+#' @param year Optional. ...
 #' @keywords ...
 #' @examples
 #' opr()
 #' @source https://api.bnm.gov.my/
 #'
 #'
-opr <- function() get_bnm_data("/opr")
+opr <- function(year) {
+  if (missing(year)){
+    get_bnm_data("/opr")
+  } else {
+    if (!(is.numeric(year) & year >= 2000)){
+      stop("Only integer values above 2000 accepted for year")
+    }
+    get_bnm_data(glue("/opr/year/{year}"))
+  }
+}
