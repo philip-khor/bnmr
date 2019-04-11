e1840fe2a223ca0e50b3c586a7fc912783bd5682
Daniel <43946777+DanielYuo@users.noreply.github.com>
2019-04-11 13:28:53 +0100

    Adding error handling
    Adding an error handling when invalid SWIFT code is given as an argument. When the error occurs, a message "Invalid SWIFT code" will be presented.

--- R/base_rate.R
+++ R/base_rate.R
@@ -9,7 +9,17 @@
 #'
 
 base_rate <- function(bank_code = ""){
-  get_bnm_data(glue("/base-rate/{bank_code}"))
+  # Checking if any argument provided to the function
+  if (is.character(bank_code)){
+    # Handle the exception for invalid params value
+    tryCatch(get_bnm_data(glue("/base-rate/{bank_code}")),
+             error = function(e){
+               print("Invalid SWIFT code")})
+  }
+  # If no argument provided, return all the base rate
+  else{
+    get_bnm_data("/base-rate/")
+  }
 }
 
 # write a print method that shows meta?
