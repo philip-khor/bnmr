921144fb12492f8c2053f4096e29ad349f0815d4
Philip Khor <35039795+philip-khor@users.noreply.github.com>
2021-10-10 17:13:59 +0800

    Merge MSB to main (#22)
    * added hex logo to the package
    
    * Move logo to man else R thinks the package needs to be compiled
    Autogenerate README.md from Rmd. note get_base_rate now doesn't work: need to investigate.
    
    * add MSB endpoints
    
    * add placeholders for remaining section 1 msb data
    
    Co-authored-by: zatch3301 <zatch.3301@gmail.com>

--- R/monthly.R
+++ R/monthly.R
@@ -77,7 +77,7 @@ get_islamic_interbank_rate <- function(date = NULL,
 #' market rates and volumes of transactions according to tenure.
 #' (2015 - present) from the BNM API.
 #' @inheritParams get_islamic_interbank_rate
-#' @param product Either "money_market_operations", "interbank" or "overall"
+#' @param product One of "money_market_operations", "interbank" or "overall"
 #' @keywords api
 #' @export
 #' @examples
@@ -89,9 +89,10 @@ get_islamic_interbank_rate <- function(date = NULL,
 #'
 
 get_interest_volume <- function(product = "money_market_operations",
-                            date = NULL,
-                            year = NULL,
-                            month = NULL) {
+                                date = NULL,
+                                year = NULL,
+                                month = NULL) {
+
   stopifnot(product %in% .products)
 
   .get_monthly_data(
