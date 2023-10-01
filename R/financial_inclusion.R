914ebc29deba5839a7aed0a1433bf25f777da4a3
Philip Khor <35039795+philip-khor@users.noreply.github.com>
2023-10-02 00:35:46 +0800

    initial financial inclusion functions (#36)
    * initial financial inclusion functions
    
    * merge documentation pages

---
+++ R/financial_inclusion.R
@@ -0,0 +1,33 @@
+#' Financial Inclusion
+#'
+#' @name financial_inclusion
+#' @keywords financial_inclusion api
+#' @examples
+#' get_financing_applied()
+#' get_financing_approved()
+#' @param year year
+#' @source https://apikijangportal.bnm.gov.my/
+#'
+NULL
+
+#' @describeIn financial_inclusion Financing Applied
+#' @export
+get_financing_applied <- function(year = NULL) {
+  path <- "/financial_inclusion/1.11"
+  if (!is_null(year)) {
+    assertthat::is.count(year)
+    path <- paste0(path, "/", as.character(year))
+  }
+  get_bnm_tbl(path)
+}
+
+#' @describeIn financial_inclusion Financing Approved
+#' @export
+get_financing_approved <- function(year = NULL) {
+  path <- "/financial_inclusion/1.12"
+  if (!is_null(year)) {
+    assertthat::is.count(year)
+    path <- paste0(path, "/", as.character(year))
+  }
+  get_bnm_tbl(path)
+}
